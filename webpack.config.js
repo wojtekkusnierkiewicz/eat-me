f145cdaf4684582f24822dead1fba2bd4651f89d
wojtekkusnierkiewicz <wojtek.kusnierkiewicz@gmail.com>
2017-12-19 21:33:29 +0100

    Adding project files

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+
+module.exports = {
+  entry: ["whatwg-fetch", "./js/zadanie01.jsx"],
+  output: {
+    filename: "./js/out.js"
+  },
+  devtool: "#eval-source-map",
+  watch: true,
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015','stage-2','react']
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './',
+    port: 3001,
+    inline: true
+  }
+}
