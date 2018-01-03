b290c74d9e9009d927a1bfd1faaffbd22adcb83a
Wojtek Kusnierkiewicz <wojtek.kusnierkiewicz@gmail.com>
2018-01-03 19:53:46 +0100

    first component with query to api

--- js/app.jsx
+++ js/app.jsx
@@ -1,10 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Input from './components/inputcomponent.jsx';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isData: false,
+      data: [],
+      text: '',
+      page: 1
+    };
+    this.getData();
+  }
+  //fetch api
+  getData(){
+    fetch(`http://www.recipepuppy.com/api/?p=${this.state.page}&q=${this.state.text}`).then(response=>{
+      if(response&&response.ok){
+        return response.json();
+      } else {
+        console.log('err');
+      }
+    }).then(data=>{
+      console.log(data.results);
+      this.setState({
+        isData: true,
+        data: data.results
+      })
+    })
+  }
+
+  request = () => {
+    this.getData()
+  }
+
+  //callback get
+  getState = (text) => {
+    this.setState({ text: text })
+  }
+
   render() {
-    return <h1> test </h1>
+    console.log(this.state.text);
+    console.log(this.state.data);
+    return this.state.isData ? (
+      <div>
+        <Input
+          text={this.state.text}
+          send={this.getState}
+          request={this.request}/>
+      </div>
+    ) : (
+      <div>
+        <img src='./images/source.gif' alt='prosze czekac'/>
+      </div>
+    )
   }
 }
 
---
+++ js/components/inputcomponent.jsx
@@ -0,0 +1,38 @@
+import React, {Component} from 'react'
+
+class Input extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: this.props.text
+    };
+  }
+
+  //getting value from input and sending it to App component in callback
+  textChange = (event) => {
+    this.setState({
+      text: event.target.value
+    }, () => {
+      this.props.send(this.state.text)
+    });
+  }
+
+  // init api search on btn
+  request = (e) => {
+    e.preventDefault();
+    this.props.request();
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <input type='text' id='searchInput' placeholder='just type and search' value={this.state.text} onChange={this.textChange}/>
+          <button onClick={this.request}>Search</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Input;
