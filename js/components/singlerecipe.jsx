bcd42d70a91eb2a87f4d6704dc514cd5c2448027
Wojtek Kusnierkiewicz <wojtek.kusnierkiewicz@gmail.com>
2018-01-15 21:49:34 +0100

    responsive and try to deal with iphone7

--- js/components/singlerecipe.jsx
+++ js/components/singlerecipe.jsx
@@ -11,6 +11,9 @@ const SingleRecipe = (props) => {
   (data.title.length > 30)
     ? (data.title = data.title.substring(0, 30) + '...')
     : (data.title = data.title);
+  (data.ingredients.length > 90)
+    ? (data.ingredients = data.ingredients.substring(0,90) + '...')
+    : (data.ingredients = data.ingredients)
   return (
     <li>
       <img src={(
